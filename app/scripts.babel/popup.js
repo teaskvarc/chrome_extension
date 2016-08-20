1c7af4e26d60a2e7a3e1c66a7a2121246d95acd7
teaskv <teaskvarc@gmail.com>
2016-08-20 14:55:47 +0200

    (added code for urls, when write something in popup window, 3 tabs automatically open)

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -2,8 +2,20 @@
 
 console.log('\'Allo \'Allo! Popup');
 
+const urls = ['http://youtube.com', 'http://vimeo.com', 'http://vid.me'];
+
+
 $('#search-btn').on('click', ()=>{
 
   console.log($('#my-input').val());
+
+  for(var i = 0; i<urls.length; i++){
+
+    //dobila v dokumentaciji od Chroma, pod Tabs
+    chrome.tabs.create({}, ()=>{
+
+
+    });
+  }
 
 });
