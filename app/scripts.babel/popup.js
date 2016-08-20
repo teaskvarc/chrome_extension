330101cdc5715c2a42fdf020b641e6b832d48024
teaskv <teaskvarc@gmail.com>
2016-08-20 15:09:28 +0200

    (added urls+term so you can search with term in 3 different urls)

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -2,18 +2,19 @@
 
 console.log('\'Allo \'Allo! Popup');
 
-const urls = ['http://youtube.com', 'http://vimeo.com', 'http://vid.me'];
+const urls = ['http://youtube.com/results?search_query=', 'http://vimeo.com/search?q=', 'http://vid.me/search?q='];
 
 
 $('#search-btn').on('click', ()=>{
 
-  console.log($('#my-input').val());
+  const term = $('#my-input').val();
 
-  for(var i = 0; i<urls.length; i++){
+  for(var i = 0; i < urls.length; i++){
 
     //dobila v dokumentaciji od Chroma, pod Tabs
-    chrome.tabs.create({}, ()=>{
-
+    chrome.tabs.create({
+      url:urls[i]+term
+    }, ()=>{
 
     });
   }
