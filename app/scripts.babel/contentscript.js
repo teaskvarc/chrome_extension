40d63ca610304106261e163303c6097a674160d3
teaskv <teaskvarc@gmail.com>
2016-08-20 14:29:51 +0200

    (added container and button that show in Trello app - but without update, added input in popup)

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -9,8 +9,8 @@ let $sidebar;
 
 function getSidebar() {
 
-  if($sidebar){
-    console.log('Found');
+  if($sidebar && $sidebar[0] !== undefined){
+    console.log('Found: ', $sidebar[0]);
     clearTimeout(timeout);
     init();
 
@@ -28,10 +28,34 @@ function getSidebar() {
 }
 getSidebar();
 
+
 // tale init pozenemo, ko je sidebar pripravljen
 
 function init() {
 
+  //naredimo container(izhajamo iz html od Trella, uporabljam jQuery)
+  let $trackerContainer = $('<div>', { class: 'window-module u-clearfix'});
+
+  // kot h3 dodamo svoj text, gumb se bo imenoval: Tracker
+  let $trackerTitle = $('<h3>', { text: 'Tracker'});
+  let $buttonContainer = $('<div>', { class: 'u-clearfix' });
+  let $button = $('<a>', { class: 'button-link', text: 'Start tracking'});
+
+  $buttonContainer.append($button);
+
+  //s tem dodamo title v container. lahko dodajamo vec stvari
+  $trackerContainer.append($trackerTitle, $buttonContainer);
+
+  // v sidebar, ki smo ga ze zgoraj poklicali dodamo se tale container
+  // ko damo .append, se nam pojavi cisto na dnu,
+  //ce zelimo, da se nam pojavi na vrhu damo .prepend
+  $sidebar.prepend($trackerContainer);
+
+  $button.on('click', ()=>{
+
+    alert('Hello user!');
+
+  });
 }
 
 
